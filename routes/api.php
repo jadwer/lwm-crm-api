83565ce2c0303e9de3c3b587bf60336d58837bb8
Gabino Ramírez <jadwer@msn.com>
2024-05-13 03:27:04 -0600

    versioning API

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,14 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
-    return $request->user();
+Route::prefix('v1')->group(function(){
+
+    Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
+        return $request->user();
+    });
+
+    Route::get('test', function(){
+        return "ok";
+    });
+
 });
