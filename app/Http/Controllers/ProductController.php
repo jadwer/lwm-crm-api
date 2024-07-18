31efb0102fcc06094952ab1dea986c4fe630efec
Gabino Ramírez <jadwer@msn.com>
2024-07-18 01:27:31 -0600

    New route for contact/estimation form

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -14,7 +14,7 @@ class ProductController extends Controller
 {
     public function index(Request $request): Response
     {
-        $products = ProductResource::collection(Product::filters($request)->paginate(5))->resource ;
+        $products = ProductResource::collection(Product::filters($request)->paginate(5)->setPath(""))->resource ;
 
         return response($products);
     }
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -7,3 +9,14 @@ Route::get('/', function () {
 });
 
 require __DIR__.'/auth.php';
+
+Route::post('contact_form', function (Request $request) : Response{
+    $res = [
+        "status" => "success",
+        "data" => [
+            "mensaje" => $request->mensaje
+        ]
+    ];
+    error_log($request);
+    return response(json_encode($res), 200)->header("Content-type","application/json");
+});
